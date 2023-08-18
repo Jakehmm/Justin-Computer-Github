7a74b75c058977a3ca8a5504212102179a9f695a
Cai-Liza <liza.cai.cics@ust.edu.ph>
2023-08-19 00:14:51 +0800

    Added on github

---
+++ personal-info-peta.py
@@ -0,0 +1,20 @@
+#personal info
+#PETA 1 Computer
+#applying strings and variables
+
+x = "Justine Wu Shi"
+b= "December 24, 2007"
+a = "15"
+s = "Philippine Cultural College"
+h = "Juan Luna Plaza 658 Meisic St."
+p = "9764148127"
+e = "de.jesus.justin.c@philippineculturalcollege.edu.ph"
+
+
+
+print("Student's Name:" , (x) ) 
+print("Birthday:", (b))
+print("Age: ", (a) )
+print("Home Address:" , (h) )
+print("Mobile Number:" , (p) )
+print("Email Address:" , (e) )
---
+++ sep-usage.py
@@ -0,0 +1,5 @@
+print("Luffy, Sabo, Ace", sep=",")
+
+print("Male","19", "Male", "22" , "Male" ,"20" ,sep="/")
+
+print("Dawn, Island, East Blue", sep="/")
