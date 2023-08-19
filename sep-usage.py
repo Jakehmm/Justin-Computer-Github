d2628a754785c4d944af59adea75cca49310d727
Cai-Liza <liza.cai.cics@ust.edu.ph>
2023-08-19 14:58:05 +0800

    Additional spaces

--- sep-usage.py
+++ sep-usage.py
@@ -1,6 +1,6 @@
 #use sep once at end 
-print("Luffy" ,"Sabo" ,"Ace", sep=",")
+print("Luffy" , " Sabo" , " Ace", sep=",")
 
 print("Male","19", "Male", "22" , "Male" ,"20" ,sep="/")
 
-print("Dawn, Island , East Blue" , sep=",")
+print(" Dawn", " Island", " East Blue", sep=",")
