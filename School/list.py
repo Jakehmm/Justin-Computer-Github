a1cac62e0d841b58d583e55ce1b9f1388f9c4c44
Skimatoshi <skimatoshi@gmail.com>
2023-10-05 20:02:40 +0800

    list change

--- School/list.py
+++ School/list.py
@@ -2,11 +2,11 @@ students = ["cesar","francis","mawee","austin","cynthia"]
 grades = [95,86,76,90,92]
 for i in range(5):
     print(f"{students[i]} = {grades[i]}")
-
+print("--------------------------")
 students = ("cesar","francis","mawee","austin","cynthia")
 grades = [95,86,76,90,92]
 
-print("--------------------------")
+
 print(students[0], "=", grades[0])
 print(students[-1], "=", grades[-1])
 print(students[-2], "=", grades[-2])
