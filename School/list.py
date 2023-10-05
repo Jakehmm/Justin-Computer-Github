5aa0babf9e919845febe1514c44a1c93441dd7f3
Skimatoshi <skimatoshi@gmail.com>
2023-10-05 20:02:20 +0800

    List changes

--- School/list.py
+++ School/list.py
@@ -3,12 +3,13 @@ grades = [95,86,76,90,92]
 for i in range(5):
     print(f"{students[i]} = {grades[i]}")
 
-#students = ["cesar","francis","mawee","austin","cynthia"]
-#grades = [95,86,76,90,92]
+students = ("cesar","francis","mawee","austin","cynthia")
+grades = [95,86,76,90,92]
 
-#print(students[0], "=", grades[0])
-#print(students[1], "=", grades[1])
-#print(students[2], "=", grades[2])
-#print(students[3], "=", grades[3])
-#print(students[4], "=", grades[4])
+print("--------------------------")
+print(students[0], "=", grades[0])
+print(students[-1], "=", grades[-1])
+print(students[-2], "=", grades[-2])
+print(students[-3], "=", grades[-3])
+print(students[-4], "=", grades[-4])
 
---
+++ School/tuples.py
@@ -0,0 +1,12 @@
+students1 = ("cesar", "francis", "mawee", "austin", "cynthia")
+grades1 = (95, 86, 76, 90, 92)
+print(students1[0], "=", grades1[0])
+print(students1[1], "=", grades1[1])
+print(students1[2], "=", grades1[2])
+print(students1[3], "=", grades1[3])
+print(students1[4], "=", grades1[4])
+
+x = 0o143
+print(oct(x))
+print(bin(x))
+print(x)
