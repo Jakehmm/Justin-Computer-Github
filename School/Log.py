2d8cbdd79d8143771368889dd1b738811f515cab
Skimatoshi <skimatoshi@gmail.com>
2023-10-11 19:09:04 +0800

    Log changes

---
+++ School/Log.py
@@ -0,0 +1,18 @@
+# Get input from the user
+#number = int(input("Enter a number: "))
+
+# Check if the number is divisible by both 2 and 3
+#if number % 2 == 0 and number % 3 == 0:
+    #print(f"{number} is divisible by both 2 and 3.")
+#else:
+    #print(f"{number} is not divisible by both 2 and 3.")
+
+# or 
+
+
+#
+y1 = input("Enter a Number: ")
+y = 12
+print(" y=" ,y)
+print(y%2==0 and y%3 ==0)
+
