2c62b9f00d420477c726ae24650ae6f0fd066ffe
Skimatoshi <skimatoshi@gmail.com>
2023-11-05 09:32:17 +0800

    idk flutter try

---
+++ python_work/floatt.py
@@ -0,0 +1,14 @@
+x = 1
+y = 2
+
+print( y/x )
+
+# Python ignores the underscore
+x = 1_000_000
+print(x)
+
+j, t ,v = 1,2,3
+print(j,t,v)
+
+michael, jackson, vibes = 0, 1, 2
+print(michael, jackson, vibes)
---
+++ python_work/integers.py
@@ -0,0 +1,9 @@
+print(5+3)
+print(11-3)
+print(4*2)
+x = round(16 / 2)
+print(x)
+
+y = 5
+message = "It's your sixteeth birthday"
+print(f"{y}, {message}")
