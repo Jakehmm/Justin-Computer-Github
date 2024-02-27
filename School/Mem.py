6e84d612652b3d72746d05a18b8a7611ad0a423d
Skimatoshi <skimatoshi@gmail.com>
2024-02-27 21:22:21 +0800

    I'm back to coding... reset reset

--- School/Mem.py
+++
@@ -1,3 +0,0 @@
-x = "H&D Information Technology Inc."
-y = (1,2,3,4)
-print(5 in y) 
--- School/sep-usage.py
+++
@@ -1,6 +0,0 @@
-#use sep once at end 
-print("Luffy" , " Sabo" , " Ace", sep=",")
-
-print("Male","19", "Male", "22" , "Male" ,"20" ,sep="/")
-
-print(" Dawn", " Island", " East Blue", sep=",")
