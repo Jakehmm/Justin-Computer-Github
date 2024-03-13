1dbcb4b4aaf0206d883f024b75e7a62091a028c0
Skimatoshi <skimatoshi@gmail.com>
2024-03-14 05:51:16 +0800

    Ok

--- CS50-Harvard/Chapter-2/Conditionals.py
+++ CS50-Harvard/Chapter-2/Conditionals.py
@@ -13,8 +13,17 @@
 != : 
 
 '''
-def main(username):
-    print(f"Hello, {username.capitalize()}")
 
-ask_name = input("What's your name? ")
-main(ask_name)
+def divide_square(to_square,to_divide) -> int:
+    if to_divide >= to_square:
+        raise ValueError("Try a lower number.")
+    if to_divide == to_square:
+        raise ValueError("No equivalent numbers, sorry")
+    return to_square **2 / to_divide
+
+square_value = int(input("Input a number you want it to be squared: "))
+divide_value = int(input("Input the number you want to divide from the squared number: "))
+
+
+result = divide_square(square_value,divide_value)
+print(result)
